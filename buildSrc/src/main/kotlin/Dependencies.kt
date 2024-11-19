4c1b3c000b02805d5bdd296dff021624345b6460
Alexander Ivanovsky <alxiw@outlook.com>
2024-11-20 02:51:40 +0300

    Fix buildScr

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,9 +1,3 @@
-object Versions {
-    const val kotlin = "1.8.10"
-    const val koin = "2.0.1"
-    const val retrofit = "2.9.0"
-    const val rxjava = "3.1.6"
-    const val glide = "4.14.2"
-    const val groupie = "2.10.1"
-    const val realm = "10.13.0"
+object Hello {
+    const val hello = "hello"
 }
